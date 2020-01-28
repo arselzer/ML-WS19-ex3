05ef8e1f6fb8af6d2a3604a502366cf127f05e20
Alexander Selzer <aselzer3@gmail.com>
2020-01-28 14:53:56 +0100

    add evasion script

--- pubfig/big/evasion.py
+++ pubfig/big/evasion.py
@@ -16,32 +16,32 @@ import skimage
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.utils import plot_model
 
+from cleverhans.attacks import FastGradientMethod
+from cleverhans.loss import CrossEntropy
+from cleverhans.utils import AccuracyReport
+from cleverhans.utils_tf import model_eval
+
 IMG_HEIGHT = 128
 IMG_WIDTH = 128
 
-image_generator = ImageDataGenerator(rescale=1./255)
-train_data = image_generator.flow_from_directory(batch_size=32,
-	directory=sys.argv[1],
-	target_size=(IMG_HEIGHT, IMG_WIDTH),
-	class_mode=None)
+#image_generator = ImageDataGenerator(rescale=1./255)
+#train_data = image_generator.flow_from_directory(batch_size=32,
+#	directory=sys.argv[1],
+#	target_size=(IMG_HEIGHT, IMG_WIDTH),
+#	class_mode=None)
 
 def load_image(filename):
 	img = tf.keras.preprocessing.image.load_img(filename, target_size=(IMG_WIDTH,IMG_HEIGHT))
 	img = tf.keras.preprocessing.image.img_to_array(img)
-	#img = np.array(img).astype("float32")/255
-	#img = skimage.transform.resize(img, (IMG_WIDTH, IMG_HEIGHT, 3))
 	img = np.expand_dims(img, axis=0) / 255
 	return img
-
-from glob import glob
-class_names = glob("persons-cropped/*")
-class_names = sorted(class_names)
-#labels = dict(zip(class_names, range(len(class_names))))
 
 labels_file = open("labels.json", "r")
 labels = json.loads(labels_file.read())
 
-print(labels)
+x = load_image(sys.argv[1])
+
+sess=tf.session()
 
 model = tf.keras.models.load_model("model.h5")
 
@@ -51,9 +51,20 @@ model.summary()
 
 #img = load_image(sys.argv[1])
 
+fgsm = FastGradientMethod(model)
+fgsm_params = {'eps': 0.3,
+                 'clip_min': 0.,
+                 'clip_max': 1.}
+adv_x = fgsm.generate(x, **fgsm_params)
+  # Consider the attack to be constant
+adv_x = tf.stop_gradient(adv_x)
+preds_adv = model(adv_x)
+
+
+
 #predictions = model.predict(img, verbose=1)
-predictions = model.predict(train_data, verbose=1)
-prediction = predictions.argmax(axis=-1)
+#predictions = model.predict(train_data, verbose=1)
+#prediction = predictions.argmax(axis=-1)
 
 print(predictions)
 print(prediction)
